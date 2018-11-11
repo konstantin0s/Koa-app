82587b3f558f1471d8e74eeaa5d62863843556bd
konstantin0s <constantintofan85@gmail.com>
2018-11-11 15:15:27 +0100

    Add koa-json

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const = require('koa');
+const Koa = require('koa');
+const json = require('koa-json');
 
 const app = new Koa();
 
-app.use(async ctx => ctx.body = 'Hello World!');
+app.use(json());
+
+app.use(async ctx => ctx.body = {msg: 'Hello World'});
 
 app.listen(3000, () => console.log('Served started...'));
