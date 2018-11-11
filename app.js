c4f3c5edc6c62f562afe3628cebcfe2bc0016a95
konstantin0s <constantintofan85@gmail.com>
2018-11-11 15:20:43 +0100

    Add koa router

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Koa = require('koa');
 const json = require('koa-json');
+const KoaRouter = require('koa-router');
 
 const app = new Koa();
+const router = new KoaRouter();
 
 app.use(json());
+//Simple Middleware
+// app.use(async ctx => ctx.body = {msg: 'Hello World'});
 
-app.use(async ctx => ctx.body = {msg: 'Hello World'});
+router.get('/test', ctx => (ctx.body = 'Hello Test'));
+
+//Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Served started...'));
